01b9c9d4b7cc9499bf300ca6782842493585a49a
niu-baobao <591866654@qq.com>
2020-01-17 09:46:28 +0800

    数据库名更改

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
     'DB_TYPE'=>'mysql',// 数据库类型
     'DB_HOST'=>'localhost',// 服务器地址
-    'DB_NAME'=>'jwkjwxmps3',// 数据库名
+    'DB_NAME'=>'jwkjwxmps',// 数据库名
     'DB_USER'=>'root',// 用户名
     'DB_PWD'=>'',// 密码
     'DB_PORT'=>3306,// 端口
